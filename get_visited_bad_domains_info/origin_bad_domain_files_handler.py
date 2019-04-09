fa029978368121097e9aab4e1bf23b51a95f0855
xixi2 <2821893735@qq.com>
2019-04-09 22:24:06 +0800

    原始域名数据文件处理

--- get_visited_bad_domains_info/origin_bad_domain_files_handler.py
+++ get_visited_bad_domains_info/origin_bad_domain_files_handler.py
@@ -1,10 +1,12 @@
-"""此文件是为了处理恶意域名数据集，这些数据集中有一些额外字段，需要提取"""
+"""
+此文件是为了处理原始的恶意域名文件，这些文件中有一些额外字段，需要提取，处理这些原始域名文件后得到恶意域名数据集
+"""
+import os
 import csv
 import pandas as pd
 
-from get_visited_bad_domains_info.convert2nd_domain import is_domain_ip
-
-ROOT_DIR = "../data_set/bad_domains/"
+from common.domains_op import is_domain_ip, keep_2nd_dom_name
+from common.domains_op import PRE_DIR
 
 
 def write2file(file, domains_set):
@@ -31,12 +33,16 @@ def csv2csv(file, url_col, time_col, type_col=-1, pattern="2019"):
     :return:
     """
     file_prefix = file.split(".")[0]
-    src_file = ROOT_DIR + "/" + file
-    dst_file = ROOT_DIR + "/" + file_prefix + "_simple_" + pattern + ".csv"
-    dst_file1 = ROOT_DIR + "/" + file_prefix + "_simple_" + pattern + ".txt"
+    src_file = PRE_DIR + file
+    dst_file = PRE_DIR + file_prefix + "_simple_" + pattern + ".csv"
+    dst_file1 = PRE_DIR + file_prefix + "_simple_" + pattern + ".txt"
     http_phrase = "http://"
-    domains_list = []
-    domains_type = []
+    port_phrase = ":"
+    domains_list, domains_type = [], []
+    domain_2nd_list = []
+    if not os.path.exists(src_file):
+        return
+    print("src_file: %s" % (src_file,))
     with open(src_file, "r") as f_out:
         f_csv = csv.reader(f_out)
         for row in f_csv:
@@ -55,6 +61,9 @@ def csv2csv(file, url_col, time_col, type_col=-1, pattern="2019"):
                     url = url[:pos]
                 if not is_domain_ip(url):
                     # print("url %s, row[1]: %s" % (url, row[1]))
+                    if url.find(port_phrase) >= 0:
+                        pos = url.find(port_phrase)
+                        url = url[:pos]
                     if url not in domains_list:
                         domains_list.append(url)
 
@@ -65,13 +74,16 @@ def csv2csv(file, url_col, time_col, type_col=-1, pattern="2019"):
                             type = row[type_col]
                         domains_type.append(type)
 
+                        # 为了给二级域名添加响应的恶意类型时需要
+                        domain_2nd_list.append(keep_2nd_dom_name(url))
+
     print("len of urls : %s, len of domains_info: %s" % (len(domains_list), len(domains_type)))
-    # write2csv(dst_file, domains_list, domains_type)
+    write2csv(dst_file, domain_2nd_list, domains_type)
     write2file(dst_file1, domains_list)
 
 
 if __name__ == '__main__':
     files = ["abuse.csv", "hosts_phishtank.csv"]
     pattern = "2017"
-    # csv2csv(files[0], 2, 1, 4)
-    csv2csv(files[1], 1, 3, -1, pattern)
+    csv2csv(files[0], 2, 1, 4)
+    # csv2csv(files[1], 1, 3, -1, pattern)
