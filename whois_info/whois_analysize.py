99f85c50709ccb9e4a25319724b1c285e22203a0
xixi2 <2821893735@qq.com>
2019-04-23 21:40:29 +0800

    分析whois信息

---
+++ whois_info/whois_analysize.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from pymongo import MongoClient
+
+from common.mongodb_op import MAL_DOMS_MONGO_DB, GOOD_DOMAINS_MONGO_DB, DOMAIN_WHOIS_MONGO_INDEX
+from common.common_whois_fields import CREATE_DATE, UPDATE_DATE, EXPIRY_DATE, VALID_DURATION
+from common.mongo_common import DOMAIN_2ND_FIELD
+from common.mongodb_op import mongo_url
+
+client = MongoClient(mongo_url)
+db_whois_bad = client[MAL_DOMS_MONGO_DB]
+db_whois_good = client[GOOD_DOMAINS_MONGO_DB]
+BAD_WHOIS_FILE = "bad_whois.csv"
+GOOD_WHOIS_FILE = "good_whois.csv"
+db_dict = {
+    0: db_whois_good,
+    1: db_whois_bad
+}
+
+
+def get_whois_info(db, mongo_index):
+    recs = db[mongo_index].find()
+    date_dict = {}
+    date_dict_list = []
+    for rec in recs:
+        domain = rec[DOMAIN_2ND_FIELD]
+        create_date = rec.get(CREATE_DATE, None)
+        expiry_date = rec.get(EXPIRY_DATE, None)
+        update_date = rec.get(UPDATE_DATE, None)
+        if create_date:
+            print("domain: %s, create_date:%s" % (domain, create_date))
+            if expiry_date:
+                print("nont create_date:%s, expiry_date:%s" % (create_date, expiry_date))
+
+        if create_date and expiry_date:
+            print(domain)
+            valid_duration = expiry_date - create_date
+            date_dict = {DOMAIN_2ND_FIELD: domain, VALID_DURATION: valid_duration}
+            date_dict_list.append(date_dict)
+    df = pd.DataFrame(date_dict_list, columns=[DOMAIN_2ND_FIELD, VALID_DURATION])
+    df.to_csv(BAD_WHOIS_FILE, index=True)
+
+
+if __name__ == '__main__':
+    domain_bad = int(input("please enter what kind of domains to query from the logs:0 for good, 1 for bad"))
+    db_whois = db_dict[domain_bad]
+    mongo_index = DOMAIN_WHOIS_MONGO_INDEX
+    get_whois_info(db_whois, mongo_index)
